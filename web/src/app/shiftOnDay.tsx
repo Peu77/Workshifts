7d6615b9d63b2355829a01784cb30acbce0e79df
peu77 <emilebert@icloud.com>
2024-06-10 21:11:52 +0200

    animation for joining and quiting a shift

--- web/src/app/shiftOnDay.tsx
+++ web/src/app/shiftOnDay.tsx
@@ -5,6 +5,7 @@ import {ShiftDay, useDeleteShiftFromDay, useJoinShiftDay, useQuitShiftDay} from 
 import {Button} from "@/components/ui/button.tsx";
 import {useMemo} from "react";
 import {useGetMe} from "@/admin/usersApi.ts";
+import {ColorRing} from "react-loader-spinner";
 
 interface ShiftDayProps {
     shiftDay: ShiftDay,
@@ -19,7 +20,7 @@ export default ({shiftDay, isAdmin, date}: ShiftDayProps) => {
     const quitShiftDayMutation = useQuitShiftDay(date);
 
     const isMeIncluded = useMemo(() => {
-        if(!getMe.data) return false
+        if (!getMe.data) return false
 
         return shiftDay.users.some(user => user.id === getMe.data.id)
     }, [shiftDay]);
@@ -46,7 +47,7 @@ export default ({shiftDay, isAdmin, date}: ShiftDayProps) => {
 
     return (
         <div
-            className={cn(shiftDay.users.length < shiftDay.shift.minEmployees ? "bg-red-200 " : "", "p-3 rounded-lg relative")}>
+            className={cn(shiftDay.users.length < shiftDay.shift.minEmployees ? "bg-red-200 " : "bg-green-200", "p-3 rounded-lg relative")}>
             {isAdmin && <MinusCircleIcon
                 onClick={() => deleteShiftFromDayMutation.mutate(shiftDay.id)}
                 className="absolute right-[-5px] top-[-5px] cursor-pointer hover:text-red-300"/>}
@@ -56,7 +57,15 @@ export default ({shiftDay, isAdmin, date}: ShiftDayProps) => {
             <p>Users: {shiftDay.users.length}/{shiftDay.shift.minEmployees}</p>
 
 
-            {isMeIncluded ? <Button className="w-full mt-4" onClick={quitShift}>quit</Button> :
+            {(joinShiftDayMutation.isPending || quitShiftDayMutation.isPending) ? <ColorRing
+                visible={true}
+                height="60"
+                width="60"
+                ariaLabel="color-ring-loading"
+                wrapperStyle={{margin: "auto"}}
+                wrapperClass="color-ring-wrapper"
+                colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+            /> : isMeIncluded ? <Button className="w-full mt-4" onClick={quitShift}>quit</Button> :
                 <Button className="w-full mt-4" variant="secondary" onClick={joinShift}>join</Button>}
         </div>
     )
